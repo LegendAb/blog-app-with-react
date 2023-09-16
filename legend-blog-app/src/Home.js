8f718daf454b83e9629639644961b3987bd7c34e
LegendAb <abraham.jonathan2015@gmail.com>
2023-09-16 23:55:06 +0100

    props

--- legend-blog-app/src/Home.js
+++ legend-blog-app/src/Home.js
@@ -1,22 +1,19 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new blog website', body: 'lorem ipsum...', author: 'legend', id: 1},
-        { title: 'Welcome to the blog!', body: 'lorem ipsum...', author: 'legend', id: 2},
+        { title: 'Welcome to the blog!', body: 'lorem ipsum...', author: 'sarah', id: 2},
         { title: 'Web dev top tips for beginners', body: 'lorem ipsum...', author: 'legend', id: 3}
     ]);
 
     
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Written by { blog.author }</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title='All Blogs!' />
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'legend')} title="Legend's blogs" />
         </div>
      );
 }
