d993abe033e1b26e0bdcd6624087457d3ed77c12
LegendAb <abraham.jonathan2015@gmail.com>
2023-09-16 01:24:45 +0100

    use state hook

--- legend-blog-app/src/Home.js
+++ legend-blog-app/src/Home.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
+
 const Home = () => {
+    const [name, setName] = useState('legend');
+    const [age, setAge] = useState(27);
 
     const handleClick = () => {
-        console.log('hello, world!')
+        setName('Abraham');
+        setAge(28);
     }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name } is { age } years old</p>
             <button onClick={handleClick}>Click me</button>
         </div>
      );
