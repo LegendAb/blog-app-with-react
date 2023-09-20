9985ef4cb86d75734016d0f36c628432b1d689d4
LegendAb <abraham.jonathan2015@gmail.com>
2023-09-21 00:33:31 +0100

    loading and error messages

--- legend-blog-app/src/Home.js
+++ legend-blog-app/src/Home.js
@@ -5,6 +5,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
 
     const [name, setName] = useState("legend");
 
@@ -17,18 +18,27 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             .then(res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setLoading(false)
-            });
+                setError(null)
+            })
+            .catch(err => {
+                setLoading(false)
+                setError(err.message) 
+            })
         }, 1000);        
     }, []);
 
     
     return ( 
         <div className="home">
+            { error && <div>{error}</div> }
             { loading && <div>Loading...</div> }
             { blogs && <BlogList blogs={blogs} title="All Blogs!" /> }
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'legend')} title="Legend's blogs" /> */}
