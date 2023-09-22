dc2bf4f16739b75ceb51de5cc0f3d1b6ebf46b17
LegendAb <abraham.jonathan2015@gmail.com>
2023-09-22 13:48:29 +0100

    link

--- legend-blog-app/src/Navbar.js
+++ legend-blog-app/src/Navbar.js
@@ -1,13 +1,15 @@
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Legend Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color: "white",
                     backgroundColor: "#800080",
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
      );
